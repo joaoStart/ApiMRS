c2709c7fab62764eaefd45600c98686acda97e02
joão victor <jvictor491@gmail.com>
2022-01-19 16:04:04 -0300

    adicionando a class finalizacao

---
+++ src/main/java/com/restapi/logapi/logApi/domain/service/FinalizacaoEntregaService.java
@@ -0,0 +1,25 @@
+package com.restapi.logapi.logApi.domain.service;
+
+import com.restapi.logapi.logApi.domain.model.Entrega;
+import com.restapi.logapi.logApi.domain.repository.EntregaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class FinalizacaoEntregaService {
+
+    private EntregaRepository entregaRepository;
+    private BuscaEntregaService buscaEntregaService;
+
+    @Transactional
+    public void finalizar(Long entregaId){
+        Entrega entrega = buscaEntregaService.buscar(entregaId);
+
+        entrega.finalizar();
+
+        entregaRepository.save(entrega);
+    }
+}
+
